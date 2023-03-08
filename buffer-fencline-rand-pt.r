dcf0cd18b46705a32226aca9d1f1f2cb5f9ab631
Echidna93 <jackx022@LAPTOP-9P1O15HA>
2023-03-08 17:14:37 -0600

    add export gpx points

--- buffer-fencline-rand-pt.r
+++ buffer-fencline-rand-pt.r
@@ -9,12 +9,11 @@ library(spatialEco)
 library(spatstat.random)
 
 nsamp=20
-earth.radius = 6371
 point.dist.cond = 50 # want meters between points
 tas_utm <- st_crs("+proj=utm +zone=15 +datum=WGS84 +units=m +no_defs")
 
 # euclidean distance function
-eucDistance<-function(P1, P2, earthRadius){
+eucDistance<-function(P1, P2){
   sqrt((((P2[1]-P1[1])^2)+((P2[2]-P1[2])^2)))
 }
 
@@ -45,35 +44,44 @@ sample.points <- sampleStratified(x=lc.fenceline.rast$lc_antler_adventures_area_
                                   na.rm=TRUE,
                                   xy=TRUE,
                                   sp=TRUE)
-s.pts<-st_as_sf(sample.points)
-filter.pts<-data.frame(x<-c(),
-                       y<-c())
-pt.list <- c()
+bad.indx.lst <- c()
 k<-1
 data.points<-list()
-for(i in 1:(nrow(s.pts)-1)){
-  p1 <- c(s.pts$x[i], s.pts$y[i])
-  for(j in (i+1):nrow(s.pts)){
-    p2 <- c(s.pts$x[j], s.pts$y[j])
-    if((i != j) & (eucDistance(p1, p2, earth.radius) < point.dist.cond)){
-       # print(eucDistance(p1,p2,earth.radius))
-       # data.point<-list(x=s.pts$x[i],y=s.pts$y[i])
-       # filter.pts<-rbind(filter.pts,data.point)
-       pt.list[k]<-i
+for(i in 1:(nrow(sample.points)-1)){
+  p1 <- c(sample.points$x[i], sample.points$y[i])
+  for(j in (i+1):nrow(sample.points)){
+    p2 <- c(sample.points$x[j], sample.points$y[j])
+    # last condition will ensure that only points with indices not already in 
+    # will go thru this condition; gets around landcover classes with smaller
+    # representations
+    if((i != j) & (eucDistance(p1, p2) < point.dist.cond) & ((! i %in% bad.indx.lst) | (! j %in% bad.indx.lst))){
+       bad.indx.lst[k]<-i
        k<-k+1
       }
     }
 }
-pt.list<-unique(pt.list)
-s.pts<-s.pts[-c(pt.list),]
-pts<-list(s.pts$x, s.pts$y)
-filter.pts <- rbind(filter.pts, pts)
+bad.indx.lst<-unique(bad.indx.lst)
+sample.points<-sample.points[-c(bad.indx.lst),]
+
+# check for filtering
+(nrow(sample.points))
+
 # filter off excess points
-(nrow(filter.pts))
-filter.pts<-filter.pts[-c(nsamp+1:nrow(s.pts)),]
-(nrow(filter.pts))
-# (nrow(s.pts))
-# filter.pts<-unique(filter.pts)
+sample.points<-sample.points[-c(nsamp+1:nrow(sample.points)),]
+
+(nrow(sample.points))
+# check for number of points
 # (nrow(filter.pts))
-plot(lc.fenceline.rast$lc_antler_adventures_area_small.tif)
-points(filter.pts, col="black")
+
+# convert points to gpx points
+
+# first convert from UTM NAD83 espg:26915 -> long lat
+long.lat <- spTransform(sample.points, CRS("+proj=longlat"))
+
+writeOGR(long.lat, dsn="./gpxTEST.gpx",
+         dataset_options="GPX_USE_EXTENSIONS=yes",layer="waypoints",driver="GPX", overwrite_layer = T)
+
+# can plot to check our point distribution
+
+# plot(lc.fenceline.rast$lc_antler_adventures_area_small.tif)
+# points(filter.pts, col="black")
